9f7817cea926f22419a829667ed1aa1158caefcc
yuni <hys990730@gmail.com>
2024-06-28 18:29:12 +0900

    feat: Image ratio Props 추가

--- src/Image/Image.tsx
+++ src/Image/Image.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export type ImageProps = React.ImgHTMLAttributes<HTMLImageElement>
+export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  ratio?: '16/9' | 'square'
+}
 
-const Image: React.FC<ImageProps> = props => {
-  return <img {...props} />
+const StyledImageWrapper = styled.div<{ ratio?: string }>`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  ${({ ratio }) => ratio === '16/9' && 'padding-top: 56.25%;' /* 16:9 비율 */}
+  ${({ ratio }) => ratio === 'square' && 'padding-top: 100%;' /* 정사각형 비율 */}
+`
+
+const StyledImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
+const Image: React.FC<ImageProps> = ({ ratio, ...props }) => {
+  return (
+    <StyledImageWrapper ratio={ratio}>
+      <StyledImage {...props} />
+    </StyledImageWrapper>
+  )
 }
 
 export default Image
