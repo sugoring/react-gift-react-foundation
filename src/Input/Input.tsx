0a114e36858e6307d687d0463fd92d01024a2e57
yuni <hys990730@gmail.com>
2024-06-28 16:57:07 +0900

    design: Input 스타일 추가

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
+const StyledInput = styled.input`
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  font-size: 16px;
+  width: 100%;
+`
+
 const Input: React.FC<InputProps> = props => {
-  return <input {...props} />
+  return <StyledInput {...props} />
 }
 
 export default Input
