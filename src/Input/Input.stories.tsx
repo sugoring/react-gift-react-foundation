e5271104ce704f0dc510c5194c6425a60d116014
yuni <hys990730@gmail.com>
2024-06-28 22:38:30 +0900

    desige: 카카오 선물하기 검색 스타일 적용

--- src/Input/Input.stories.tsx
+++ src/Input/Input.stories.tsx
@@ -11,25 +11,25 @@ const Template: StoryFn<InputProps> = args => <Input {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  placeholder: 'Default Input',
+  placeholder: '검색어를 입력해주세요.',
 }
 
 export const Small = Template.bind({})
 Small.args = {
   size: 12,
-  placeholder: 'Small Input',
+  placeholder: 'Small Input - 검색어를 입력해주세요.',
 }
 
 export const Large = Template.bind({})
 Large.args = {
   size: 20,
-  placeholder: 'Large Input',
+  placeholder: 'Large Input - 검색어를 입력해주세요.',
 }
 
 export const Responsive = Template.bind({})
 Responsive.args = {
   size: undefined,
-  placeholder: 'Responsive Input',
+  placeholder: 'Responsive Input - 검색어를 입력해주세요.',
 }
 
 export const Disabled = Template.bind({})
