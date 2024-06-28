92e3fe7ea0f944f1333e44c13206f59b01419a69
yuni <hys990730@gmail.com>
2024-06-28 16:47:32 +0900

    feat: Input.tsx 컴포넌트 초기 설정

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,5 +1,14 @@
-const myFunction = () => {
-  console.log('This is a function')
+import React, { InputHTMLAttributes } from 'react'
+import './Input.css'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  disabled?: boolean
+  invalid?: boolean
+  size?: 'small' | 'medium' | 'large' | 'responsive'
 }
 
-export default myFunction
+const Input: React.FC<InputProps> = ({ disabled, invalid, size = 'medium', ...props }) => {
+  return <input className={`input ${size} ${invalid ? 'invalid' : ''}`} disabled={disabled} {...props} />
+}
+
+export default Input
