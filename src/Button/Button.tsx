fc4ca47183f620c3db92ed89e97626dc09b1ca26
yuni <hys990730@gmail.com>
2024-06-28 14:45:14 +0900

    feat: 기본 Button 컴포넌트 구현
    - 기본 구조 생성
    - Button 컴포넌트와 패딩 스타일만을 정의함

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,16 @@
-const myFunction = () => {
-  console.log('This is a function')
+import React from 'react'
+import styled from 'styled-components'
+
+// 기본 스타일을 설정한 StyledButton 컴포넌트
+const StyledButton = styled.button`
+  padding: 10px 20px; // 버튼의 패딩 설정
+`
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+// 기본 Button 컴포넌트
+const Button: React.FC<ButtonProps> = props => {
+  return <StyledButton {...props} /> // StyledButton 컴포넌트를 반환
 }
 
-export default myFunction
+export default Button
