fe406cf2504075f1fc8dde227ed1abf3a5a7077f
yuni <hys990730@gmail.com>
2024-06-28 16:58:34 +0900

    feat: Input.stories.tsx 구현
    - Input 컴포넌트에 대한 Storybook 설정 파일을 추가

--- src/Input/Input.stories.tsx
+++ src/Input/Input.stories.tsx
@@ -1,49 +1,15 @@
 import React from 'react'
-import { Meta, Story } from '@storybook/react'
+import { StoryFn, Meta } from '@storybook/react'
 import Input, { InputProps } from './Input'
 
 export default {
   title: 'Components/Input',
   component: Input,
-  argTypes: {
-    disabled: { control: 'boolean' },
-    invalid: { control: 'boolean' },
-    size: {
-      control: { type: 'select', options: ['small', 'medium', 'large', 'responsive'] },
-    },
-  },
 } as Meta
 
-const Template: Story<InputProps> = args => <Input {...args} />
+const Template: StoryFn<InputProps> = args => <Input {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  disabled: false,
-  invalid: false,
-  size: 'medium',
-  placeholder: 'Enter text...',
-}
-
-export const Disabled = Template.bind({})
-Disabled.args = {
-  disabled: true,
-  invalid: false,
-  size: 'medium',
-  placeholder: 'Disabled input',
-}
-
-export const Invalid = Template.bind({})
-Invalid.args = {
-  disabled: false,
-  invalid: true,
-  size: 'medium',
-  placeholder: 'Invalid input',
-}
-
-export const Responsive = Template.bind({})
-Responsive.args = {
-  disabled: false,
-  invalid: false,
-  size: 'responsive',
-  placeholder: 'Responsive input',
+  placeholder: 'Default Input',
 }
