52c87245e06165e7ef12efb72aec1c9be8acce7c
yuni <hys990730@gmail.com>
2024-06-28 16:58:40 +0900

    feat: Input.tsx Size Props 추가
    - size Props에 따라 크기를 다르게 설정

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  size?: 'small' | 'medium' | 'large' | 'responsive'
+}
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<InputProps>`
   padding: 10px;
   border-radius: 4px;
   border: 1px solid #ccc;
   font-size: 16px;
   width: 100%;
+
+  ${({ size }) =>
+    size === 'small' &&
+    css`
+      font-size: 12px;
+      padding: 8px;
+    `}
+
+  ${({ size }) =>
+    size === 'medium' &&
+    css`
+      font-size: 16px;
+      padding: 10px;
+    `}
+
+  ${({ size }) =>
+    size === 'large' &&
+    css`
+      font-size: 20px;
+      padding: 12px;
+    `}
+
+  ${({ size }) =>
+    size === 'responsive' &&
+    css`
+      width: 100%;
+      @media (min-width: 768px) {
+        width: auto;
+      }
+    `}
 `
 
-const Input: React.FC<InputProps> = props => {
-  return <StyledInput {...props} />
+const Input: React.FC<InputProps> = ({ size, ...props }) => {
+  return <StyledInput size={size} {...props} />
+}
+
+Input.defaultProps = {
+  size: 'medium',
 }
 
 export default Input
