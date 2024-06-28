a5a029b8e6c060a0e3c31f7f25c5d4912ace0035
yuni <hys990730@gmail.com>
2024-06-28 18:40:13 +0900

    feat: Grid 컴포넌트에 대한 Storybook 구현

--- src/Grid/Grid.stories.tsx
+++ src/Grid/Grid.stories.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Meta, StoryFn } from '@storybook/react'
+import { StoryFn, Meta } from '@storybook/react'
 import Grid from './Grid'
 
 export default {
   title: 'Components/Grid',
   component: Grid,
-} as Meta<typeof Grid>
+} as Meta
 
-const Template: StoryFn<typeof Grid> = args => <Grid {...args} />
+const Template: StoryFn = args => (
+  <Grid {...args}>
+    <div style={{ backgroundColor: 'lightgray', height: '100px' }}>{'Item 1'}</div>
+    <div style={{ backgroundColor: 'lightgray', height: '100px' }}>{'Item 2'}</div>
+    <div style={{ backgroundColor: 'lightgray', height: '100px' }}>{'Item 3'}</div>
+    <div style={{ backgroundColor: 'lightgray', height: '100px' }}>{'Item 4'}</div>
+  </Grid>
+)
 
 export const Default = Template.bind({})
 Default.args = {
-  // 적절한 기본 props를 여기다 설정
+  columns: 2,
+  gap: '16px',
 }
