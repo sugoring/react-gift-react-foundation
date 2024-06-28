701a19ae39782b66176a11b31ec491406cd7a5a9
yuni <hys990730@gmail.com>
2024-06-28 18:30:24 +0900

    feat: Image radius Props 추가

--- src/Image/Image.tsx
+++ src/Image/Image.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   ratio?: '16/9' | 'square'
+  radius?: number | 'circle'
 }
 
 const StyledImageWrapper = styled.div<{ ratio?: string }>`
@@ -13,19 +14,30 @@ const StyledImageWrapper = styled.div<{ ratio?: string }>`
   ${({ ratio }) => ratio === 'square' && 'padding-top: 100%;' /* 정사각형 비율 */}
 `
 
-const StyledImage = styled.img`
+const StyledImage = styled.img<{ radius?: number | string }>`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   object-fit: cover;
+
+  ${({ radius }) =>
+    typeof radius === 'number' &&
+    css`
+      border-radius: ${radius}px;
+    `}
+  ${({ radius }) =>
+    radius === 'circle' &&
+    css`
+      border-radius: 50%;
+    `}
 `
 
-const Image: React.FC<ImageProps> = ({ ratio, ...props }) => {
+const Image: React.FC<ImageProps> = ({ ratio, radius, ...props }) => {
   return (
     <StyledImageWrapper ratio={ratio}>
-      <StyledImage {...props} />
+      <StyledImage radius={radius} {...props} />
     </StyledImageWrapper>
   )
 }
