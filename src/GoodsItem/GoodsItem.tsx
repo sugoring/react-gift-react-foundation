1c5fddced251ecdb0ec537c5ba0640972433a4e4
yuni <hys990730@gmail.com>
2024-06-28 18:36:25 +0900

    design: GoodsItem 컴포넌트 스타일 추가

--- src/GoodsItem/GoodsItem.tsx
+++ src/GoodsItem/GoodsItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export interface GoodsItemProps {
   imageSrc: string
@@ -7,17 +8,48 @@ export interface GoodsItemProps {
   amount: number
 }
 
+const GoodsItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  width: 200px;
+`
+
+const GoodsImage = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+`
+
+const GoodsSubtitle = styled.p`
+  font-size: 14px;
+  color: #888;
+`
+
+const GoodsTitle = styled.h3`
+  font-size: 18px;
+  margin: 8px 0;
+`
+
+const GoodsAmount = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+`
+
 const GoodsItem: React.FC<GoodsItemProps> = ({ imageSrc, subtitle, title, amount }) => {
   return (
-    <div>
-      <img src={imageSrc} alt={title} />
-      <p>{subtitle}</p>
-      <h3>{title}</h3>
-      <p>
+    <GoodsItemWrapper>
+      <GoodsImage src={imageSrc} alt={title} />
+      <GoodsSubtitle>{subtitle}</GoodsSubtitle>
+      <GoodsTitle>{title}</GoodsTitle>
+      <GoodsAmount>
         {amount}
         {'원'}
-      </p>
-    </div>
+      </GoodsAmount>
+    </GoodsItemWrapper>
   )
 }
 
