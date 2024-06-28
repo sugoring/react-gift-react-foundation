db0a50d9768508f0f76b7096f9eeabac3413a739
yuni <hys990730@gmail.com>
2024-06-28 14:49:55 +0900

    feat: Button storybook 기본 설정
    - 버튼의 기본 텍스트 설정

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Meta, Story } from '@storybook/react'
-import Button from './Button'
+import { Story, Meta } from '@storybook/react'
+import Button, { ButtonProps } from './Button'
 
+// Storybook 설정
 export default {
-  title: 'Button',
-  component: Button,
+  title: 'Components/Button', // 스토리의 제목
+  component: Button, // 사용할 컴포넌트
 } as Meta
 
-const Template: Story = args => <Button {...args} />
+const Template: Story<ButtonProps> = args => <Button {...args} />
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  children: 'Default Button', // 버튼의 기본 텍스트 설정
+}
