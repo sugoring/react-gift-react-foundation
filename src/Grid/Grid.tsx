41a87064bf74631d22b47bbb237602b0b0a4d898
yuni <hys990730@gmail.com>
2024-06-28 18:39:50 +0900

    feat: Grid 컴포넌트 구현

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,5 +1,23 @@
-const myFunction = () => {
-  console.log('This is a function')
+import React from 'react'
+import styled from 'styled-components'
+
+interface GridProps {
+  columns: number
+  gap: string
 }
 
-export default myFunction
+const StyledGrid = styled.div<GridProps>`
+  display: grid;
+  grid-template-columns: repeat(${props => props.columns}, 1fr);
+  gap: ${props => props.gap};
+`
+
+const Grid: React.FC<GridProps> = ({ columns, gap, children }) => {
+  return (
+    <StyledGrid columns={columns} gap={gap}>
+      {children}
+    </StyledGrid>
+  )
+}
+
+export default Grid
