c0f1cc8c2e9b1c254b33ab589959a99382ef633d
yuni <hys990730@gmail.com>
2024-06-28 15:17:03 +0900

    feat: Button.tsx 기본 값 설정
    - theme prop이 필수가 아니지만 defaultProps에 해당 속성을 선언하지 않았기 때문에 발생하는 오류
    - defaultProps를 추가하여 기본 값을 설정함

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -57,8 +57,14 @@ const StyledButton = styled.button<ButtonProps>`
     `}
 `
 
-const Button: React.FC<ButtonProps> = ({ theme = 'primary', size = 'medium', ...props }) => {
+const Button: React.FC<ButtonProps> = ({ theme, size, ...props }) => {
   return <StyledButton theme={theme} size={size} {...props} />
+}
+
+// defaultProps를 추가하여 기본 값 설정
+Button.defaultProps = {
+  theme: 'primary',
+  size: 'medium',
 }
 
 export default Button
