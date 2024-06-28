a76ffa416345fe03be964f27c7ddbe111c8e4838
yuni <hys990730@gmail.com>
2024-06-28 18:28:43 +0900

    feat: Image 컴포넌트의 Storybook 구현

--- src/Image/Image.stories.tsx
+++ src/Image/Image.stories.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Meta, StoryFn } from '@storybook/react'
-import Image from './Image'
+import { StoryFn, Meta } from '@storybook/react'
+import Image, { ImageProps } from './Image'
 
 export default {
   title: 'Components/Image',
   component: Image,
-} as Meta<typeof Image>
+} as Meta
 
-const Template: StoryFn<typeof Image> = args => <Image {...args} />
+const Template: StoryFn<ImageProps> = args => <Image {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  // 적절한 기본 props를 여기다 설정
+  src: 'https://via.placeholder.com/300',
+  alt: 'Default Image',
 }
